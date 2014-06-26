888c6d2c7fef74c06ae2ba1e6f3c49585c190945
andi34 <skate4life@gmx.de>
2014-06-26 16:34:47 +0200

    Initial

---
+++ tools/p3110_to_p3113.sh
@@ -0,0 +1,4 @@
+#!/tmp/busybox sh
+/tmp/busybox sed -i 's/p3110/p3113/g' /system/build.prop
+/tmp/busybox sed -i 's/P3110/P3113/g' /system/build.prop
+/tmp/busybox sed -i 's/espressowifixx/espressowifiue/g' /system/build.prop
