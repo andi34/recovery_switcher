2521706a603cec77b08c5187b1d99681c7bd3d52
andi34 <skate4life@gmx.de>
2014-06-26 16:52:33 +0200

    initial

---
+++ tools/p5110_to_p5113.sh
@@ -0,0 +1,4 @@
+#!/tmp/busybox sh
+/tmp/busybox sed -i 's/p5110/p5113/g' /system/build.prop
+/tmp/busybox sed -i 's/P5110/P5113/g' /system/build.prop
+/tmp/busybox sed -i 's/espresso10wifixx/espresso10wifibby/g' /system/build.prop
